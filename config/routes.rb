cd344a0a16f50261e645cbf2676160dda9d1f143
Kevin Salazar <kskevin170@yahoo.com>
2016-10-28 16:39:49 -0700

    adds routes and reference for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'users/' => 'users#index'
+
+  get 'users/new' => 'users#new', as: :new_user
+  get 'users/:id' => 'users#show'
+  post 'users/' => 'users#create'
+  get 'users/:id/edit' => 'users#edit'
+  patch 'users/:id' => 'users#update'
+
+  get 'users/:id/edit' => 'users#edit', as: :edit_user
+  delete 'users/:id' => 'users#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
