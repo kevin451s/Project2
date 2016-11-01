e8d43a1692faa2f8a0bae66dffde43a066dcb923
Kevin Salazar <kskevin170@yahoo.com>
2016-11-01 09:07:26 -0700

    adds links to home page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ApplicationRecord
   has_attached_file :image, styles: {
     small: "64x64",
     med: "100x100",
-    large: "200x200"
+    large: "360x250"
   }
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
